b9bac1c25394433039b162a5c7fdc7fef8b96cbd
Harsha Srikara <harshasrikara@gmail.com>
2021-10-02 13:01:19 -0500

    Add query caching

--- fetchData/getOfficers.js
+++ fetchData/getOfficers.js
@@ -1,31 +1,59 @@
 import admin from "../firebase/nodeApp";
+const db = admin.firestore();
 
 export const getOfficers = async (query) => {
-  const db = admin.firestore();
 
   let officers = (await db.collection("total").doc("allinone").get()).data();
-  const officerList = [];
 
   if (query) {
-    await db
-      .collection("officer")
-      .where("role_list", "array-contains", query)
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          const data = doc.data();
-          officerList.push({
-            id: doc.id,
-            name: data.name,
-          });
-        });
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
+
+    // check if we have queried for this before
+    const res = await db
+      .collection("total")
+      .doc("allinone")
+      .collection("rolequery")
+      .where("query", "==", query)
+      .get();
+
+    // if queried before look up results
+    if (!res.empty) {
+      officers.officers = res.docs[0].data().officers;
+    }
+    // else perform the query and save the results for next time
+    else {
+      const officerList = await queryRole(query);
+
+      await db.collection("total").doc("allinone").collection("rolequery").add({
+        query: query,
+        officers: officerList,
       });
-    console.log(officerList);
-    officers.officers = officerList;
+
+      officers.officers = officerList;
+    }
   }
 
   return officers;
+};
+
+const queryRole = async (query) => {
+  const officerList = [];
+
+  await db
+    .collection("officer")
+    .where("role_list", "array-contains", query)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        officerList.push({
+          id: doc.id,
+          name: data.name,
+        });
+      });
+    })
+    .catch((error) => {
+      console.log("Error getting documents: ", error);
+    });
+
+  return officerList;
 };
