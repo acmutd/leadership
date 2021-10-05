c9eb1e8a6a5452c30d7fbcbaef5658e16d67e0ea
Harsha Srikara <harshasrikara@gmail.com>
2021-10-05 12:58:40 -0500

    sentry env variable

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { Provider } from "next-auth/client";
 import "../styles/global.css";
 
 Sentry.init({
-  dsn: "https://b57fd9ee7a7940c28312b27badb97af5@o457361.ingest.sentry.io/5992299",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NODE_ENV,
   
   // Set tracesSampleRate to 1.0 to capture 100%
