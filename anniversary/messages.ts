fc5c4c2affdd0b36a5ead51ec4b6d63bfca9d8ec
Harsha Srikara <harshasrikara@gmail.com>
2021-12-08 17:47:05 -0600

    feat: add anniversary github action (#20)

---
+++ anniversary/messages.ts
@@ -0,0 +1,16 @@
+const messages = [
+    "Cheers to your special day. Happy Anniversary!",
+    "It's time to celebrate all over again. Happy Anniversary!",
+    "Success never comes in a day; it comes with time and hard work, and you are an example of that. Happy Anniversary!",
+    "With each passing year, you bring creativity and a strong work ethic to the team. Happy Anniversary!",
+    "Congrats on another year of hard work and success! Happy Anniversary!",
+    "This is to remind you that you have come a long way, and your contributions have continued to inspire us. Happy Anniversary!",
+    "It feels just like yesterday since you started working with us, whereas it's already been another year. Time flies with you. Happy Anniversary!",
+    "It’s tough to stay put in the same place for a very long time. However, maybe the word “Tough” wasn’t in your dictionary, and you have sailed together with us through all the harsh and good times. Happy Anniversary!",
+    "It takes only one to make a difference, and you have displayed it every day since you have joined us. Happy Anniversary!",
+    "Your positive attitude inspires everyone here to give their best. Happy Anniversary!",
+    "Filled with a positive vibe, confidence, honesty, and fearlessness, we couldn’t have found anyone better than you for our team. Happy Anniversary!",
+    "I extend my heartiest congratulations to you on setting yet another milestone in your journey. Happy Anniversary!",
+]
+
+export default messages;
