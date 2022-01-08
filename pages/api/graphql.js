fc91992098a843b4b50f95a594b4e1ed376c839f
Harsha Srikara <harshasrikara@gmail.com>
2022-01-08 15:20:37 -0600

    add graphql api support

---
+++ pages/api/graphql.js
@@ -0,0 +1,26 @@
+import { ApolloServer } from "apollo-server-micro";
+import Cors from "micro-cors";
+import { typeDefs } from "./schemas";
+import { resolvers } from "./resolvers";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const cors = Cors();
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+const startServer = apolloServer.start();
+
+export default cors(async (req, res) => {
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+
+  await startServer;
+  await apolloServer.createHandler({ path: "/api/graphql" })(req, res);
+});
