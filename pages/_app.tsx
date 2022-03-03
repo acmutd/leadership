234b9977d4f8040886e32bef13d6662e0db14b19
Harsha Srikara <harshasrikara@gmail.com>
2022-03-03 13:21:16 -0600

    chore: remove unused dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
 import { Provider } from "next-auth/client";
 import { ThemeProvider } from "next-themes";
-import { AppProps } from 'next/app';
-import UserProvider from "../context/userContext";
+import { AppProps } from "next/app";
 import "../styles/global.css";
 
 // Custom App to wrap it with context provider
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Provider session={pageProps.session}>
-        <ThemeProvider defaultTheme="system">
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </Provider>
-    </UserProvider>
+    <Provider session={pageProps.session}>
+      <ThemeProvider defaultTheme="system">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
   );
 }
