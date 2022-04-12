ba65845d36d82f9d0f1176ffc06877bc8e8d251a
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2022-04-11 20:18:03 -0500

    replace doppler w/ hardcoded env

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,10 +1,10 @@
-import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
-import { ApolloServer } from "apollo-server-micro";
-import jwt from "jsonwebtoken";
-import Cors from "micro-cors";
-import { resolvers } from "./resolvers";
-import { typeDefs } from "./schemas";
-import getEnv from "../../util/env";
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ApolloServer } from 'apollo-server-micro';
+import jwt from 'jsonwebtoken';
+import Cors from 'micro-cors';
+import { resolvers } from './resolvers';
+import { typeDefs } from './schemas';
+// import getEnv from '../../util/env';
 
 export const config = {
   api: {
@@ -20,24 +20,31 @@ const apolloServer = new ApolloServer({
   introspection: true,
   plugins: [ApolloServerPluginLandingPageLocalDefault({ footer: false })],
   context: async ({ req }) => {
-    const env = await getEnv();
-    const public_key = env.LEADERSHIP_RSA_PUBLIC_KEY.replace(/\\n/gm, '\n');
+    // const env = await getEnv();
+    const public_key = process.env.LEADERSHIP_RSA_PUBLIC_KEY.replace(
+      /\\n/gm,
+      '\n'
+    );
 
-    const decoded_token = jwt.verify(req.headers.authorization.split(" ")[1], public_key, { issuer: env.NEXTAUTH_URL });
+    const decoded_token = jwt.verify(
+      req.headers.authorization.split(' ')[1],
+      public_key,
+      { issuer: process.env.NEXTAUTH_URL }
+    );
     return {
       ...(decoded_token as jwt.JwtPayload),
-    }
-  }
+    };
+  },
 });
 
 const startServer = apolloServer.start();
 
 export default cors(async (req, res) => {
-  if (req.method === "OPTIONS") {
+  if (req.method === 'OPTIONS') {
     res.end();
     return false;
   }
 
   await startServer;
-  await apolloServer.createHandler({ path: "/api/graphql" })(req, res);
+  await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
 });
