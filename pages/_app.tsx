d8057638c45c31716863b65272ef418660dcf587
Harsha Srikara <harshasrikara@gmail.com>
2022-04-13 18:43:56 -0500

    feat: add progress bar for route changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,16 @@ import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
 import "../styles/global.css";
 
+// add import
+import nProgress from "nprogress";
+import { Router } from "next/router";
+import "../styles/nprogress.css"
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
+// end
+
 // Custom App to wrap it with context provider
 export default function App({ Component, pageProps }: AppProps) {
   return (
